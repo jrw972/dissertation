77f6f3d73c5f229941e270f5759ca70a33702887
Justin R. Wilson <jrw972@gmail.com>
2016-09-20 20:33:14 -0500

    Slides for defense

---
+++ dissertation/spell_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+aspell -c abstract.tex
+aspell -c async_partitions.tex
+aspell -c background.tex
+aspell -c conclusion.tex
+aspell -c icc.tex
+aspell -c implementation.tex
+aspell -c introduction.tex
+aspell -c language.tex
+aspell -c model.tex
+aspell -c paper.tex
+aspell -c scheduler.tex
+aspell -c sync_partitions.tex
